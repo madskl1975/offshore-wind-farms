612ffab97bbda05606a39479583a015f5d5b7614
Mads Kromann-Larsen <mads.kromann-larsen@ft.dk>
2022-11-25 14:18:14 +0100

    NavBar and search

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,21 +1,18 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Button, Form } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 export default function MainMenu() {
   return (
-    <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
+    <Navbar bg="primary" variant="dark" sticky="top" expand="lg">
       <Container>
         <Navbar.Brand>
           <NavLink className="text-decoration-none text-white" to="/">
-            React Bootstap SPA
+            Offshore Wind Farms
           </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className="nav-link" to="/">
-              Home
-            </NavLink>
             <NavLink className="nav-link" to="/about">
               About
             </NavLink>
@@ -23,6 +20,15 @@ export default function MainMenu() {
               Contact
             </NavLink>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
