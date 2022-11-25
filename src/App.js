f2a0130769596f85fd61a965d6d6de39f40820ea
Mads Kromann-Larsen <mads.kromann-larsen@ft.dk>
2022-11-25 13:46:51 +0100

    init4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import MainMenu from "./components/MainMenu";
 import AboutPage from "./pages/AboutPage";
-
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <>
       <MainMenu />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,20 @@
+import { Container, Button } from "react-bootstrap";
+export default function ContactPage() {
+  return (
+    <>
+      <header className="p-5 mb-4 bg-light">
+        <Container className="py-5">
+          <h1 className="display-5 fw-bold">Contact Page</h1>
+          <p className="col-md-8 fs-4">
+            This is a Single Page App starter template with React and the UI
+            Library Bootstrap.
+          </p>
+          <p>Go to the React Bootstrap Docs.</p>
+          <Button variant="primary" size="lg">
+            Example button
+          </Button>
+        </Container>
+      </header>
+    </>
+  );
+}
