d4da965186a779b19df23410080999603dd2c5d4
Mads Kromann-Larsen <mads.kromann-larsen@ft.dk>
2022-11-25 12:04:30 +0100

    init app 2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Offshore Wind Farms</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
