91f65cd4e196a99113a3076ad5a170a31c6ed960
Mads Kromann-Larsen <mads.kromann-larsen@ft.dk>
2022-11-25 11:57:22 +0100

    init app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Offshore Wind Farms</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
